c057e3756a310871b89d67744e6ddd3e967ba98d
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-08-23 22:01:38 +0530

    [master] initial build 📦

---
+++ precache-manifest.b8d5b2a3daea3322bb26e5d61b32468e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb613d870a42279648bd9329ac6b0ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a02b03ccec716390a6e",
+    "url": "/static/css/main.8c118b17.chunk.css"
+  },
+  {
+    "revision": "1439c7bb8da89f92b8b0",
+    "url": "/static/js/2.635580e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.635580e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a02b03ccec716390a6e",
+    "url": "/static/js/main.0b41533a.chunk.js"
+  },
+  {
+    "revision": "df77f6b41eaed11cde78",
+    "url": "/static/js/runtime-main.a984057d.js"
+  }
+]);
